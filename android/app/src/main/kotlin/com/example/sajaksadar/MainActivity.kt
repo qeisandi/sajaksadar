6a4f70fbff56ab8f54f1713ab5219b8df9e204a1
qeis andi <qeis.andi02@gmail.com>
2025-07-22 22:31:05 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/sajaksadar/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.sajaksadar
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/login regis/regis.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Regis extends StatefulWidget {
+  static String id = "/regis";
+  const Regis({super.key});
+
+  @override
+  State<Regis> createState() => _RegisState();
+}
+
+class _RegisState extends State<Regis> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
